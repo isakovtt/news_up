406c7acdaafb798ddf094bb7184b4f3e15c19251
isakovtt <turqay.isak@gmail.com>
2024-03-06 07:52:11 -0800

    first commit

---
+++ lib/utils/constants/app_texts.dart
@@ -0,0 +1,20 @@
+
+class AppTexts {
+  AppTexts._();
+
+  static const skip = 'skip';
+  static const login = 'login';
+  static const getStarted = 'Get Started';
+  static const joinNewsup = 'Join Newsup.';
+  static const welcomeBackToNewsup = 'Welcome back to Newsup.';
+  static const orSignUpWith = 'Or sign up with';
+  static const orSignInWith = 'Or sign in with';
+  static const signInWithGoogle = 'Sign in with Google';
+  static const signInWithApple = 'Sign in with Apple';
+  static const forgotPassword = 'Forgot password?';
+  static const rememberMe = 'Remember me';
+  static const signIn = 'Sign in';
+  static const signUp = 'Sign up';
+  static const alreadyHaveAnAccount = 'Already have an account?';
+  static const dontHaveAnAccount = 'Don\'t have an account?';
+}
