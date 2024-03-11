0d5bc72ace6e0de12c2875546095ed3ffc928c9c
SananMahmudov <sananmahmudov957@gmail.com>
2024-03-11 19:03:57 +0400

    padding duzeldi

--- lib/utils/constants/app_paddings.dart
+++ lib/utils/constants/app_paddings.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class AppPaddings {
   const AppPaddings._();
 
-  static EdgeInsets get lr24 => const EdgeInsets.only(left: 24, right: 24);
-  static EdgeInsets get lr22 => const EdgeInsets.only(left: 22, right: 22);
+  static EdgeInsets get h24 => const EdgeInsets.symmetric(horizontal: 24);
+  static EdgeInsets get h22 => const EdgeInsets.symmetric(horizontal: 22);
 }
