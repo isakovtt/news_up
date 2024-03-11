da306c1344fafbaa5cb8f43c93546ace909b95a3
SananMahmudov <sananmahmudov957@gmail.com>
2024-03-11 19:15:43 +0400

    padding duzeldi

--- lib/utils/constants/app_paddings.dart
+++ lib/utils/constants/app_paddings.dart
@@ -5,4 +5,5 @@ class AppPaddings {
 
   static EdgeInsets get h24 => const EdgeInsets.symmetric(horizontal: 24);
   static EdgeInsets get h22 => const EdgeInsets.symmetric(horizontal: 22);
+  static EdgeInsets get h20 => const EdgeInsets.symmetric(horizontal: 20);
 }
