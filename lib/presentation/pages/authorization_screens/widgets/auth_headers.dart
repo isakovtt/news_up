d0ffec33f0591e07304ea6b58e769af943bb297b
SananMahmudov <sananmahmudov957@gmail.com>
2024-03-16 16:51:46 +0400

    auth screens repaired

--- lib/presentation/pages/authorization_screens/widgets/auth_headers.dart
+++ lib/presentation/pages/authorization_screens/widgets/auth_headers.dart
@@ -11,7 +11,7 @@ class AuthHeaders extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Column(crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           children: [
