af071f21ed0e51674b7e9b7eb789bfa896bf0bdc
SananMahmudov <sananmahmudov957@gmail.com>
2024-03-16 15:28:04 +0400

    imports repaired

--- lib/presentation/pages/authorization_screens/identification/identify_type_screen/identify_type_screen.dart
+++ lib/presentation/pages/authorization_screens/identification/identify_type_screen/identify_type_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:newsup_app/presentation/pages/authorization_screens/identification/email_identity_screen/email_identity_screen.dart';
+
 import 'package:newsup_app/presentation/pages/authorization_screens/identification/identify_type_screen/widgets/identify_type_boxes.dart';
-import 'package:newsup_app/presentation/pages/authorization_screens/identification/phone_identification_screen/phone_identity_screen.dart';
 import 'package:newsup_app/presentation/pages/authorization_screens/widgets/auth_headers.dart';
 import 'package:newsup_app/presentation/pages/authorization_screens/widgets/sign_in_up_button.dart';
 import 'package:newsup_app/utils/constants/app_paddings.dart';
-import 'package:newsup_app/utils/helpers/navigate.dart';
 import '../../../../../utils/constants/app_texts.dart';
 
 class IdentifyTypeScreen extends StatelessWidget {
