d0490610307a1c1eef159f32e6ec214873fdfe8a
isakovtt <turqay.isak@gmail.com>
2024-04-30 14:53:40 -0700

    post share firebase

--- lib/presentation/pages/preview/widgets/preview_publish_button.dart
+++ lib/presentation/pages/preview/widgets/preview_publish_button.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:newsup_app/cubits/write_news/write_news_cubit.dart';
 
+import '../../../../cubits/write_news/write_news_cubit.dart';
 import '../../../../utils/constants/app_texts.dart';
+import '../../../../utils/helpers/navigate.dart';
+import '../../../widgets/global_progress_indicator.dart';
 import '../../../widgets/inside_colored_button.dart';
+import '../../profile/profile_screen.dart';
 
 class PreviewPublishButton extends StatelessWidget {
   const PreviewPublishButton({super.key});
@@ -11,15 +14,24 @@ class PreviewPublishButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cubit = context.read<WriteNewsCubit>();
-    return BlocBuilder<WriteNewsCubit, WriteNewsState>(
+    return BlocConsumer<WriteNewsCubit, WriteNewsState>(
+      listener: (context, state) {},
       builder: (context, state) {
-        return InsideColoredButton(
-          text: AppTexts.publish,
-          width: double.infinity,
-          onTap: () {
-            cubit.sendData();
-          },
-        );
+        if (state is WriteNewsLoading) {
+          return const GlobalProgressIndicator();
+        } else {
+          return InsideColoredButton(
+            text: AppTexts.publish,
+            width: double.infinity,
+            onTap: () {
+              cubit.sendData();
+              Navigate.navigateReplacePush(
+                context,
+                const ProfileScreen(),
+              );
+            },
+          );
+        }
       },
     );
   }
