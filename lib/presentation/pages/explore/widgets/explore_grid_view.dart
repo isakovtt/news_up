cf6b8be72b8149ee45d4b93f21505261680db1fd
isakovtt <turqay.isak@gmail.com>
2024-05-10 17:26:22 -0700

    timeago

--- lib/presentation/pages/explore/widgets/explore_grid_view.dart
+++ lib/presentation/pages/explore/widgets/explore_grid_view.dart
@@ -1,12 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:newsup_app/presentation/pages/detail/detail_news_screen.dart';
-import 'package:newsup_app/utils/helpers/navigate.dart';
 
-import '../../../../data/models/hot_news_model.dart';
 import '../../../../utils/constants/app_paddings.dart';
+import '../../../../utils/extensions/time_ago_extension.dart';
+import '../../../../utils/helpers/navigate.dart';
 import '../../../widgets/news_item.dart';
+import '../../detail/detail_news_screen.dart';
 
 class ExploreGridView extends StatelessWidget {
   const ExploreGridView({super.key});
@@ -14,59 +14,58 @@ class ExploreGridView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const SizedBox.shrink();
-          }
-          final posts = snapshot.data!.docs;
-          return Padding(
-            padding: AppPaddings.h24,
-            child: GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: posts.length,
-              scrollDirection: Axis.vertical,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                mainAxisExtent: 200.h,
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-              ),
-              itemBuilder: (context, index) {
-                final news = hotNews[index];
-                final post = posts[index];
-
-                return StreamBuilder(
-                    stream: FirebaseFirestore.instance
-                        .collection('channels')
-                        .where('channel', isEqualTo: post['channel'])
-                        .snapshots(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return const SizedBox.shrink();
-                      }
-                      // final channels = snapshot.data!.docs[index];
-                      final channels = snapshot.data!.docs.first.data();
-
-                      return NewsItem(
-                        image: post['newsPhoto'],
-                        headlineText: post['newsTitle'],
-                        sourceIcon: channels['logo'],
-                        sourceName: post['channel'],
-                        categoryText: post['category'],
-                        sharedTimeText: news.sharedTimeText,
-                        onTap: () {
-                          return Navigate.navigatePush(
-                            context,
-                            DetailNewsScreen(postId: post.id),
-                          );
-                        },
-                      );
-                    });
-              },
+      stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const SizedBox.shrink();
+        }
+        final posts = snapshot.data!.docs;
+        return Padding(
+          padding: AppPaddings.h24,
+          child: GridView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: posts.length,
+            scrollDirection: Axis.vertical,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisExtent: 200.h,
+              crossAxisCount: 2,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
             ),
-          );
-        });
+            itemBuilder: (context, index) {
+              final post = posts[index];
+              final Timestamp timestamp = post['time'];
+              return StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('channels')
+                    .where('channel', isEqualTo: post['channel'])
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const SizedBox.shrink();
+                  }
+                  final channels = snapshot.data!.docs.first.data();
+                  return NewsItem(
+                    image: post['newsPhoto'],
+                    headlineText: post['newsTitle'],
+                    sourceIcon: channels['logo'],
+                    sourceName: post['channel'],
+                    categoryText: post['category'],
+                    sharedTimeText: timestamp.toDate().toTimeAgo(),
+                    onTap: () {
+                      return Navigate.navigatePush(
+                        context,
+                        DetailNewsScreen(postId: post.id),
+                      );
+                    },
+                  );
+                },
+              );
+            },
+          ),
+        );
+      },
+    );
   }
 }
--- lib/presentation/pages/home/widgets/home_news_item_list_view.dart
+++ lib/presentation/pages/home/widgets/home_news_item_list_view.dart
@@ -1,12 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:newsup_app/presentation/pages/detail/detail_news_screen.dart';
-import 'package:newsup_app/utils/helpers/navigate.dart';
 
-import '../../../../data/models/news_headline_model.dart';
 import '../../../../utils/constants/app_paddings.dart';
+import '../../../../utils/extensions/time_ago_extension.dart';
+import '../../../../utils/helpers/navigate.dart';
 import '../../../widgets/custom_basic_list_tile.dart';
+import '../../detail/detail_news_screen.dart';
 
 class HomeNewsItemListView extends StatelessWidget {
   const HomeNewsItemListView({super.key});
@@ -14,56 +14,56 @@ class HomeNewsItemListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const SizedBox.shrink();
-          }
-          final posts = snapshot.data!.docs;
-          return Padding(
-            padding: AppPaddings.h24,
-            child: ListView.separated(
-              padding: AppPaddings.b16,
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              itemCount: posts.length,
-              physics: const NeverScrollableScrollPhysics(),
-              separatorBuilder: (context, index) => 16.verticalSpace,
-              itemBuilder: (context, index) {
-                final post = posts[index];
-                final headline = newsheadline[index];
-                return StreamBuilder(
-                    stream: FirebaseFirestore.instance
-                      .collection('channels')
-                      .where('channel', isEqualTo: post['channel'])
+      stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const SizedBox.shrink();
+        }
+        final posts = snapshot.data!.docs;
+        return Padding(
+          padding: AppPaddings.h24,
+          child: ListView.separated(
+            padding: AppPaddings.b16,
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            itemCount: posts.length,
+            physics: const NeverScrollableScrollPhysics(),
+            separatorBuilder: (context, index) => 16.verticalSpace,
+            itemBuilder: (context, index) {
+              final post = posts[index];
+              final Timestamp timestamp = post['time'];
+              return StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('channels')
+                    .where('channel', isEqualTo: post['channel'])
                     .snapshots(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return const SizedBox.shrink();
-                      }
-                    final channels = snapshot.data!.docs.first.data();
-
-                      // final channels = snapshot.data!.docs[index];
-                      return GlobalBasicListTile(
-                        image: post['newsPhoto'],
-                        categoryName: post['category'],
-                        title: post['newsTitle'],
-                        sourceIcon: channels['logo'],
-                        sourceName: post['channel'] + ' News',
-                        timeText: headline.timeText,
-                        commentText: post['commentsCount'].toString(),
-                        hasSource: true,
-                        onTap: () {
-                          Navigate.navigatePush(
-                            context,
-                             DetailNewsScreen(postId: post.id),
-                          );
-                        },
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const SizedBox.shrink();
+                  }
+                  final channels = snapshot.data!.docs.first.data();
+                  return GlobalBasicListTile(
+                    image: post['newsPhoto'],
+                    categoryName: post['category'],
+                    title: post['newsTitle'],
+                    sourceIcon: channels['logo'],
+                    sourceName: post['channel'] + ' News',
+                    timeText: timestamp.toDate().toTimeAgo(),
+                    commentText: post['commentsCount'].toString(),
+                    hasSource: true,
+                    onTap: () {
+                      Navigate.navigatePush(
+                        context,
+                        DetailNewsScreen(postId: post.id),
                       );
-                    });
-              },
-            ),
-          );
-        });
+                    },
+                  );
+                },
+              );
+            },
+          ),
+        );
+      },
+    );
   }
 }
