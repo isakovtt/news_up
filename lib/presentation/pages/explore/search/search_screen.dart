1932d546ed935f0f4cb518b26976d302eb757c77
isakovtt <turqay.isak@gmail.com>
2024-05-14 15:42:36 -0700

    search firebase duzeldi

--- lib/presentation/pages/explore/search/search_screen.dart
+++ lib/presentation/pages/explore/search/search_screen.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-import 'search_app_bar.dart';
+import '../../../../utils/constants/app_assets.dart';
+import '../../../../utils/constants/app_paddings.dart';
+import '../../../../utils/constants/app_text_styles.dart';
+import '../../../../utils/constants/app_texts.dart';
+import 'search_result_view.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: SearchAppBar(),
-    );
+    return Scaffold(
+        appBar: AppBar(
+      centerTitle: true,
+      scrolledUnderElevation: 0,
+      automaticallyImplyLeading: false,
+      // leading: Padding(
+      //   padding: AppPaddings.l24,
+      //   child: Center(
+      //     child: GestureDetector(
+      //       onTap: () => Navigate.navigatePop(
+      //         context,
+      //         const ExploreScreen(),
+      //       ),
+      //       child: SvgPicture.asset(
+      //         AppAssets.arrowNarrowLeft,
+      //         height: 32.h,
+      //         width: 32.w,
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      title: Text(
+        AppTexts.explore,
+        style: AppTextStyles.greyScale900s16W700,
+      ),
+      actions: [
+        Padding(
+          padding: AppPaddings.h24,
+          child: GestureDetector(
+            onTap: () {
+              showSearch(
+                context: context,
+                delegate: CustomSearch(),
+              );
+            },
+            child: SvgPicture.asset(
+              AppAssets.search,
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
--- lib/presentation/pages/explore/widgets/explore_tabbar_view.dart
+++ lib/presentation/pages/explore/widgets/explore_tabbar_view.dart
@@ -1,32 +1,32 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import '../../../../utils/constants/app_paddings.dart';
-import '../author/author_list_view.dart';
-import '../stories/explore_stories_list_view.dart';
-import '../topic/topic_list_view.dart';
+// import '../../../../utils/constants/app_paddings.dart';
+// import '../author/author_list_view.dart';
+// import '../stories/explore_stories_list_view.dart';
+// import '../topic/topic_list_view.dart';
 
-class ExploreTabbarView extends StatelessWidget {
-  const ExploreTabbarView({super.key});
+// class ExploreTabbarView extends StatelessWidget {
+//   const ExploreTabbarView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: TabBarView(
-        children: <Widget>[
-          SingleChildScrollView(
-            child: Padding(
-              padding: AppPaddings.t10,
-              child: const ExploreStoriesListView(),
-            ),
-          ),
-          const SingleChildScrollView(
-            child: TopicListView(),
-          ),
-          const SingleChildScrollView(
-            child: AuthorListView(),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Expanded(
+//       child: TabBarView(
+//         children: <Widget>[
+//           SingleChildScrollView(
+//             child: Padding(
+//               padding: AppPaddings.t10,
+//               // child: const ExploreStoriesListView(),
+//             ),
+//           ),
+//           const SingleChildScrollView(
+//             // child: TopicListView(),
+//           ),
+//           const SingleChildScrollView(
+//             // child: AuthorListView(),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
